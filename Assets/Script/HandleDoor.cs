f95c1380a51df214aa03d4e2de16bab6d719b6d9
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2025-06-24 15:32:07 -0400

    Trees have been planted

--- Assets/Script/HandleDoor.cs
+++ Assets/Script/HandleDoor.cs
@@ -4,10 +4,17 @@ public class HandleDoor : MonoBehaviour
 {
     public GameManager gm;
     public float amountOfCoinsNeededToOpenDoor;
+    public Material portalMaterial;
+
+    Renderer _renderer;
+    Material _originalMaterial;
+    bool _isDoorUnlocked = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _renderer = GetComponent<Renderer>();
+        _originalMaterial = _renderer.material;
     }
 
     // Update is called once per frame
@@ -22,11 +29,24 @@ public class HandleDoor : MonoBehaviour
         {
             if(gm.GetTotalScore() == amountOfCoinsNeededToOpenDoor)
             {
-                Destroy(gameObject);
+                HandleUnlocking();
             } else
             {
                 Debug.Log("Door cannot be entered");
             }
+        }
+    }
+
+    void HandleUnlocking()
+    {
+        if (!_isDoorUnlocked)
+        {
+            _isDoorUnlocked = true;
+            _renderer.material = portalMaterial;
+        }
+        else
+        {
+            Debug.Log("You Win. Perfect");
         }
     }
 }
